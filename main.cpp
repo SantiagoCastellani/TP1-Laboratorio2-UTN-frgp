4bbc0902bdb445ff4aecadcced72a91eff8cde81
SantiagoCastellani <monitocastellani@gmail.com>
2023-09-16 16:45:11 -0300

    inicio

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/// TRABAJO PRACTICO COLABORATIVO
+/// LABORATORIO DE COMPUTACIÓN 2
+/// UTN - FRGP (PUENTES)
+/// COMISIÓN:
+
+/// GRUPO N°:
+/// INTEGRANTES:
+    //  1):
+    //  2):
+
+#include "Menu.h"
+
+int main()
+{
+    SetConsoleTitle("TRABAJO PRACTICO GRUPAL");
+    MenuPrincipal();
+}
